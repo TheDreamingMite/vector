baef5af7a01271d21f3845ee0e5a536174f4c87c
Владимир <vladimir.tikhonov02@mail.ru>
2023-08-29 00:44:47 +0300

    реализован способ со статическими массивами

--- vector/Source.cpp
+++ vector/Source.cpp
@@ -14,16 +14,45 @@ int main() {
 	cin >> column;
 
 	int m[100][100];
+	int v[100];
 
 	for (int i = 0; i < line; i++) {
 		for (int j = 0; j < column; j++) {
-			cout << "matrix [" << i << "][" << j << "] = ";
+			cout << "matrix [" << i+1 << "][" << j+1 << "] = ";
 			cin >> m[i][j];
-			cout << endl;
 		}
 	}
 	
-	int vector;
-	cout << "Enter number of line matrix: ";
-	cin >> line;
+	cout << "Enter number of vector:" << endl;
+	for (int i = 0; i < column; i++) {
+		cout << "vector [" << i+1 << "] = ";
+		cin >> v[i];
+	}
+
+	cout << "Introduced matrix:" << endl;
+	for (int i = 0; i < line; i++) {
+		for (int j = 0; j < column; j++) {
+			cout << m[i][j] << " ";
+		}
+		cout << endl;
+	}
+
+	cout << "Introduced vector:" << endl;
+	for (int i = 0; i < column; i++) {
+		cout << v[i];
+		cout << endl;
+	}
+
+	int r[100];
+	for (int i = 0; i < line; i++) {
+		r[i] = 0;
+		for (int j = 0; j < column; j++) {
+			r[i] += m[i][j] * v[j];
+		}
+	}
+
+	cout << "rezult:" << endl;
+	for (int i = 0; i < line; i++) {
+		cout << r[i] << endl;
+	}
 }
